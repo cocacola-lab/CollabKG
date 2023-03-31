62b3c54846a4e53c5a2d7632c04b4558699dea19
threeColorFr <1950097440@qq.com>
2023-03-31 21:00:58 +0800

    try: re auto

--- client/src/features/project/utils.jsx
+++ client/src/features/project/utils.jsx
@@ -3,7 +3,8 @@
     If ratio < 4.5 -> sets font to black, otherwise sets to white.
 */
 export const getFontColour = (colour) => {
-  const hexToRgb = (hex) =>
+  try{
+    const hexToRgb = (hex) =>
     hex
       .replace(
         /^#?([a-f\d])([a-f\d])([a-f\d])$/i,
@@ -13,26 +14,29 @@ export const getFontColour = (colour) => {
       .match(/.{2}/g)
       .map((x) => parseInt(x, 16));
 
-  const luminance = (r, g, b) => {
-    let a = [r, g, b].map((v) => {
-      v /= 255;
-      return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
-    });
-    return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
-  };
+    const luminance = (r, g, b) => {
+      let a = [r, g, b].map((v) => {
+        v /= 255;
+        return v <= 0.03928 ? v / 12.92 : Math.pow((v + 0.055) / 1.055, 2.4);
+      });
+      return a[0] * 0.2126 + a[1] * 0.7152 + a[2] * 0.0722;
+    };
 
-  const contrast = (rgb1, rgb2) => {
-    let lum1 = luminance(rgb1[0], rgb1[1], rgb1[2]);
-    let lum2 = luminance(rgb2[0], rgb2[1], rgb2[2]);
-    let brightest = Math.max(lum1, lum2);
-    let darkest = Math.min(lum1, lum2);
-    return (brightest + 0.05) / (darkest + 0.05);
-  };
+    const contrast = (rgb1, rgb2) => {
+      let lum1 = luminance(rgb1[0], rgb1[1], rgb1[2]);
+      let lum2 = luminance(rgb2[0], rgb2[1], rgb2[2]);
+      let brightest = Math.max(lum1, lum2);
+      let darkest = Math.min(lum1, lum2);
+      return (brightest + 0.05) / (darkest + 0.05);
+    };
 
-  const ratioWhite = contrast(hexToRgb(colour), [255, 255, 255]);
-  const ratioBlack = contrast(hexToRgb(colour), [0, 0, 0]);
+    const ratioWhite = contrast(hexToRgb(colour), [255, 255, 255]);
+    const ratioBlack = contrast(hexToRgb(colour), [0, 0, 0]);
 
-  return ratioWhite > ratioBlack ? "white" : "black";
+    return ratioWhite > ratioBlack ? "white" : "black";
+  } catch(e){
+    return "white";
+  }
 };
 
 /*
